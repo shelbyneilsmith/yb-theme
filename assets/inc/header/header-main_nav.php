b507c0c8ed2681518c3b6bab79eae25376749056
Shelby Neil Smith <shelbyneilsmith@gmail.com>
2016-01-26 22:28:57 -0600

    Moved YB Theme to own repo

---
+++ assets/inc/header/header-main_nav.php
@@ -0,0 +1,3 @@
+<div id="navigation" class="clearfix">
+	<?php wp_nav_menu( array('theme_location' => 'main-nav', 'container' => 'nav', 'container_id' => 'main-nav-header', 'container_class' => "main-nav drop-menu" )); ?>
+</div>
